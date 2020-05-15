cedb85926df195916ae49a370c8f6b5b280b4bd9
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-05-15 16:07:19 +0100

    test_add_or_remove_cash__add passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,4 +1,4 @@
-
+require("pry")
 
 def pet_shop_name(pet_shop)
     shop_name = pet_shop[:name]
@@ -10,9 +10,18 @@ def total_cash(pet_shop)
     return shop_cash_holdings
 end
 
-
-# def test_total_cash
-#     sum = total_cash(@pet_shop)
-#     assert_equal(1000, sum)
+#refactor to use above function
+def add_or_remove_cash(pet_shop, amount_to_add_or_remove)
+    pet_shop[:admin][:total_cash] += amount_to_add_or_remove
+end
+# def test_add_or_remove_cash__add
+#     add_or_remove_cash(@pet_shop,10)
+#     cash = total_cash(@pet_shop)
+#     assert_equal(1010, cash)
 # end
 
+  # def test_add_or_remove_cash__remove
+  #   add_or_remove_cash(@pet_shop,-10)
+  #   cash = total_cash(@pet_shop)
+  #   assert_equal(990, cash)
+  # end
