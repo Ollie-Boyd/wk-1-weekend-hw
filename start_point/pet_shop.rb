c29db687c854299638104fcb80d3e057ec0b22c6
Ollie Boyd <ollyboyd@hotmail.co.uk>
2020-05-15 14:03:34 +0100

    start_point: test_total_cash passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,7 +5,14 @@ def pet_shop_name(pet_shop)
     return shop_name
 end
 
+def total_cash(pet_shop)
+    shop_cash_holdings = pet_shop[:admin][:total_cash]
+    return shop_cash_holdings
+end
 
 
-
+# def test_total_cash
+#     sum = total_cash(@pet_shop)
+#     assert_equal(1000, sum)
+# end
 
